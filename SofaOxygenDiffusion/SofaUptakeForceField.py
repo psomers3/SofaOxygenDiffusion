b89a367a17cabdd55067fa90baeebf567376ae66
psomers3 <peter.somers@isys.uni-stuttgart.de>
2021-05-17 11:31:57 +0200

    initial commit

---
+++ SofaOxygenDiffusion/SofaUptakeForceField.py
@@ -0,0 +1,111 @@
+# coding: utf8
+import Sofa
+import numpy as np
+from numba import jit
+
+
+@jit(nopython=True)
+def _addForce(f, c, c_prior, d_O2SaturationConstant, edgeUptakeCoefficient, edges):
+    x = c
+    o2_coef = d_O2SaturationConstant
+    for i in range(0, len(edges)):
+        v0 = edges[i][0]
+        v1 = edges[i][1]
+        c_prior_average = (c_prior[v1] + c_prior[v0]) / 2
+        x_average = (x[v1] + x[v0]) / 2  # average the values on each vertex
+        dp2 = (edgeUptakeCoefficient[i] / (o2_coef + c_prior_average)) * x_average
+        f[v1] += dp2
+        f[v0] += dp2
+
+    return f
+
+
+@jit(nopython=True)
+def _addDForce(df, dx, c_prior, kFactor, o2_coef, edgeUptakeCoefficient, edges):
+        for i in range(len(edges)):
+            v0 = edges[i][0]
+            v1 = edges[i][1]
+            dx_average = (dx[v1] + dx[v0]) / 2  # average th values on each vertex
+            c_average = (c_prior[v1] + c_prior[v0]) / 2
+            dp2 = (edgeUptakeCoefficient[i] / (o2_coef + c_average)) * dx_average * kFactor
+            df[v1] += dp2
+            df[v0] += dp2
+        return df
+
+
+class UptakeForceField(Sofa.Core.ForceFieldVec1d):
+    """Implementation of Oxygen uptake in python"""
+
+    def __init__(self, *args, **kwargs):
+        Sofa.Core.ForceFieldVec1d.__init__(self, *args, **kwargs)
+        self.d_O2SaturationConstant = 2.5  # in mmHG
+        self.sim_time = 0
+
+    def init(self):
+        self.mechanicalState = self.getContext().getMechanicalState()
+        self.c_prior = self.mechanicalState.position.array().copy()
+        self.topology = self.getContext().getMeshTopology()
+        self.edges = self.topology.edges.array()
+        self.nbEdges = len(self.edges)
+        self.edgeUptakeCoefficient = np.zeros(self.nbEdges, dtype=np.float32)
+        self.tetra = self.topology.tetrahedra.array()
+        self.sim_time = 0
+        if self.d_tetra_uptake_coefs is None:
+            self.set_tetra_uptake_coefficients(np.zeros(len(self.tetra), dtype=float))
+        self.computeEdgeDiffusionCoefficient()
+
+    def set_tetra_uptake_coefficients(self, coefs):
+        self.d_tetra_uptake_coefs = coefs
+        try:
+            self.computeEdgeDiffusionCoefficient()
+        except KeyError:
+            # not initialized yet
+            pass
+
+    def computeEdgeDiffusionCoefficient(self):
+        nbTetra = len(self.tetra)
+        position = self.topology.position.array()
+        point = np.zeros((4, 3))
+        shapeVector = np.zeros((4, 3))
+        local_edges_in_tetrahedron = np.array([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]])
+
+        for i in range(0, nbTetra):
+            tetrahedron = self.tetra[i]
+            for j in range(0, 4):
+                point[j] = position[tetrahedron[j]]
+
+            # compute 6 times the rest volume
+            volume = np.dot(np.cross(point[1] - point[0], point[2] - point[0]), point[0] - point[3])
+
+            # store shape vectors
+            for j in range(0, 4):
+                if (j % 2) == 0:
+                    shapeVector[j] = -np.cross(point[(j + 2) % 4] - point[(j + 1) % 4],
+                                               point[(j + 3) % 4] - point[(j + 1) % 4]) / volume
+                else:
+                    shapeVector[j] = np.cross(point[(j + 2) % 4] - point[(j + 1) % 4],
+                                              point[(j + 3) % 4] - point[(j + 1) % 4]) / volume
+
+            diff2 = self.d_tetra_uptake_coefs[i] * np.absolute(volume) / 6
+
+            edges_in_tetrahedron = self.topology.getEdgesInTetrahedron(i)
+            for j in range(0, 6):
+                k = local_edges_in_tetrahedron[j][0]
+                l = local_edges_in_tetrahedron[j][1]
+                dot_product = np.dot(shapeVector[k], shapeVector[l])
+                val2 = dot_product * diff2
+                self.edgeUptakeCoefficient[edges_in_tetrahedron[j]] += val2
+
+    def addForce(self, m, force, c, vel):
+        if self.sim_time != self.getContext().getTime():
+            self.c_prior = self.mechanicalState.position.array().copy()
+            self.sim_time = self.getContext().getTime()
+        with force.writeableArray() as f:
+            return _addForce(f, c.value, self.c_prior, self.d_O2SaturationConstant, self.edgeUptakeCoefficient, self.edges)
+
+    def addDForce(self, params, dforce, dc):
+        if self.sim_time != self.getContext().getTime():
+            self.c_prior = self.mechanicalState.position.array().copy()
+            self.sim_time = self.getContext().getTime()
+        with dforce.writeableArray() as df:
+            return _addDForce(df, dc.value, self.c_prior, params['kFactor'], self.d_O2SaturationConstant, self.edgeUptakeCoefficient, self.edges)
---
+++ example/example1.py
@@ -0,0 +1,160 @@
+# coding: utf8
+import Sofa
+import SofaRuntime
+import numpy as np
+from qtpy.QtWidgets import *
+from qtpy.QtCore import *
+from QSofaGLViewTools import QSofaGLView, QSofaViewKeyboardController
+from SofaOxygenDiffusion.SofaUptakeForceField import UptakeForceField
+
+
+def createScene(node):
+    node.addObject("RequiredPlugin", name="SofaOpenglVisual")  # visual stuff
+    node.addObject("RequiredPlugin", name="SofaLoader")  # geometry loaders
+    node.addObject("RequiredPlugin", name="SofaSimpleFem")  # diffusion fem
+    node.addObject("RequiredPlugin", name="SofaBoundaryCondition")  # constraints
+    node.addObject("RequiredPlugin", name="SofaEngine")  # Box Roi
+    node.addObject("RequiredPlugin", name="SofaImplicitOdeSolver")  # implicit solver
+    node.addObject("RequiredPlugin", name="SofaExplicitOdeSolver")  # explicit solver
+    node.addObject("RequiredPlugin", name="SofaMiscForceField")  # meshmatrix
+    node.addObject("RequiredPlugin", name="SofaGeneralEngine")  # TextureInterpolation
+    node.addObject("RequiredPlugin", name="CImgPlugin")  # for loading a bmp image for texture
+    node.addObject("RequiredPlugin", name="SofaBaseLinearSolver")
+    node.addObject("RequiredPlugin", name="SofaGeneralVisual")
+    node.addObject("RequiredPlugin", name="LMConstraint")
+    node.addObject("RequiredPlugin", name="SofaGeneralLoader")
+
+    node.dt = 10  # such a low diffusion coefficient makes it take forever to diffuse
+    node.gravity = [0, 0, 0]
+    node.addObject('VisualStyle', displayFlags="showAll showWireframe")
+    node.addObject('FreeMotionAnimationLoop')
+    node.addObject('GenericConstraintSolver', maxIterations=1000, tolerance=0.0001)
+
+    mesh=node.addObject('MeshGmshLoader', name='3D_mesh', filename='TetraMesh.msh')
+    mesh.position=mesh.position.array()*1e-6
+    topology = node.addObject('TetrahedronSetTopologyContainer', name='topology', src='@3D_mesh', tags='mechanics')
+    topology.init()  # done so we can read how many vertices
+    node.addObject('MechanicalObject', template='Vec3d', name='3D_DOFs', src='@3D_mesh', tags='mechanics')
+    node.addObject('TetrahedronSetGeometryAlgorithms', template='Vec3d', name='GeomAlgo', tags='mechanics')
+
+    oxygen_node = node.addChild('oxygen')
+    oxygen_node.addObject('EulerImplicitSolver', name="Euler", firstOrder=True, tags="oxygen", rayleighStiffness=0.1,
+                          rayleighMass=0.1, trapezoidalScheme=False)
+    oxygen_node.addObject('CGLinearSolver', name="CG", iterations=1000, tolerance=1.0e-10, threshold=0, tags="oxygen")
+    # manually create a mechanical object to initialize values to zero.
+    global temps
+    loaded_temps = np.full(topology.nbPoints.value, 0)
+    temps = oxygen_node.addObject('MechanicalObject', template="Vec1d", name='oxygen_DOFs',
+                                  position=loaded_temps.tolist(), tags="oxygen")
+    tumor_roi = node.addObject('BoxROI', name="tumor", box=[-1e-4, -1e-4, -1e-4, 1e-4, 1e-4, 1e-4], drawBoxes=True,
+                               position="@3D_DOFs.position", tetrahedra="@topology.tetrahedra")
+    tumor_roi.init()
+    topology_tetra = topology.tetrahedra.array()
+    tumor_indices = tumor_roi.tetrahedronIndices.array()
+
+    uptake_coef_array = np.full(len(topology_tetra), 0, dtype=np.float32)
+    uptake_coef_array[tumor_indices] = np.full(len(tumor_indices), 0.2, dtype=np.float32)
+
+    ForceField = UptakeForceField(name="UptakeField")
+    ForceField.set_tetra_uptake_coefficients(uptake_coef_array)
+    global FEM
+    FEM = oxygen_node.addObject(ForceField)
+    oxygen_node.addObject("TetrahedronDiffusionFEMForceField", name='diffusion', template='Vec1d',
+                          tagMechanics='mechanics', tags='oxygen')
+    oxygen_node.addObject('UncoupledConstraintCorrection')
+
+    oxygen_node.addObject('MeshMatrixMass', template="Vec1d,double", name="Mass", lumping=0, massDensity=1e-12,
+                          printLog=False, tags="oxygen")
+
+    visual_node = oxygen_node.addChild('visual_node')
+    global texture
+    texture = visual_node.addObject('TextureInterpolation', template="Vec1d", name="EngineInterpolation",
+                                    input_states="@../oxygen_DOFs.position",
+                                    input_coordinates="@../../3D_DOFs.position",
+                                    min_value=0, max_value=2.5, manual_scale=1, drawPotentiels=True, showIndicesScale=0)
+    visual_node.addObject('OglModel', template="Vec3d", name="oglPotential",
+                          texcoords="@EngineInterpolation.output_coordinates", texturename="textures/heatColor.bmp",
+                          scale3d=[1, 1, 1],
+                          material="Default Diffuse 1 1 1 1 0.5 Ambient 1 1 1 1 0.3 Specular 0 0.5 0.5 0.5 1 Emissive\
+                          0 0.5 0.5 0.5 1 Shininess 0 45 No texture linked to the material No bump texture linked to\
+                          the material ")
+    visual_node.addObject('IdentityMapping', input="@../../3D_DOFs", output="@oglPotential")
+
+    node.addObject("InteractiveCamera",
+                   name="camera",
+                   position=[1.21156216e-03,  1.96106548e-04, -4.01305086e-05],
+                   orientation=[-0.08777735,  0.74885405,  0.10808765,  0.64794275],
+                   lookAt=[0, 0, 0],
+                   distance=37,
+                   fieldOfView=45)
+
+    links = node.addObject('BoxROI', name="box-links", box=[-2.6e-4, -2.6e-4, 2.6e-4, 2.6e-4, 2.6e-4, 2.4e-4],
+                           drawBoxes=True, position="@3D_DOFs.position")
+
+    # set initial temp on left side
+    links.init()
+    current_temp = temps.position.array().copy()
+    current_temp[links.indices.array()] = 2.5
+    temps.position = current_temp.tolist()
+    oxygen_node.addObject('FixedConstraint', name='flow_in', template="Vec1d", indices="@../box-links.indices")
+
+    for i in range(len(temps.position.array())):
+        stopper = oxygen_node.addObject('StopperConstraint', name='oxygen_low', template="Vec1d", index=i, min=1e-6,
+                                        max=200)
+
+    return node
+
+
+if __name__ == '__main__':
+    class SimAnimator(QObject):
+        step_finished = Signal()
+
+        def __init__(self, root_node: Sofa.Core.Node):
+            super(SimAnimator, self).__init__()
+            self.node = root_node
+            self.step_finished.connect(self.step_sim, Qt.QueuedConnection)
+            self.running = False
+
+        def step_sim(self):
+            if not self.running:
+                return
+            max = np.max(temps.position.array())
+            min = np.min(temps.position.array())
+            if max > min:
+                texture.min_value = min
+                texture.max_value = max
+            print(f'min: {min}    max: {max}')
+            self.blockSignals(True)
+            Sofa.Simulation.animate(self.node, self.node.getDt())
+            Sofa.Simulation.updateVisual(self.node)
+            self.blockSignals(False)
+            self.step_finished.emit()
+
+    app = QApplication(['sofa_app'])  # viewer engine
+
+    root_node = Sofa.Core.Node("root")
+    createScene(root_node)  # create sofa scene
+    Sofa.Simulation.init(root_node)
+    viewer = QSofaGLView(root_node, camera=root_node.camera)
+
+    view_control = QSofaViewKeyboardController(translate_rate_limit=0.01)
+    view_control.set_viewers([viewer])
+    view_control.start_auto_update()  # update view continously
+    viewer.show()
+
+    sim = SimAnimator(root_node)
+
+    def key_pressed(event):
+        key = event.key()
+        if key == Qt.Key_Space:
+            if sim.running:
+                sim.running = False
+            else:
+                sim.running = True
+                sim.step_finished.emit()
+        elif key == Qt.Key_R:
+            Sofa.Simulation.reset(root_node)
+
+    viewer.set_background_color([0, 0, 0, 1])
+    viewer.key_pressed.connect(key_pressed)
+    app.exec()
